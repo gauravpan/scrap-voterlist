bfb6ec04b45dd53fecde2be25b1ca8c7c61c2464
gauravpan <gauravpandey2058@gmail.com>
2022-02-02 18:54:10 +0545

    entry index.ts added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('test')
+import { getDistricts, submitForm } from "./submitForm";
+import fs from 'fs'
+
+// submitForm()
+const provinces = ['1', '2', '3', '4', '5']
+
+provinces.forEach(async (province) => {
+    let res = await getDistricts(province)
+    console.log(res.data)
+    // fs.writeFileSync(`../raw/district_by_provinces/${province}`, res.data.result);
+})
