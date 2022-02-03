2ce578ab248f25e8baa6d8697b5a2b69bc3cc305
gauravpan <gauravpandey2058@gmail.com>
2022-02-03 18:36:09 +0545

    tests

--- src/tests/all.test.ts
+++ src/tests/all.test.ts
@@ -7,40 +7,40 @@ import { getWards } from "../getWards";
 import { getVoterNames } from "../getVoterNames";
 
 jest.setTimeout(500000)
-// test('getStates', async () => {
-//     let vdcs = await getStates(URL);
-//     console.log(vdcs)
-//     expect(vdcs).toBeTruthy()
-// })
+test('getStates', async () => {
+    let states = await getStates(URL);
+    console.log(states)
+    expect(states).toBeTruthy()
+})
 
-// test('getDistrict', async () => {
-//     let vdcs = await getDistricts(URL, '5');
-//     console.log(vdcs)
-//     expect(vdcs).toBeTruthy()
-// })
+test('getDistrict', async () => {
+    let districts = await getDistricts(URL, '5');
+    console.log(districts)
+    expect(districts).toBeTruthy()
+})
 
 
 
-// test('getVDC', async () => {
-//     let vdcs = await getVDCs(URL, '5', '54');
-//     console.log(vdcs)
-//     expect(vdcs).toBeTruthy()
-// })
-
-// test('getWard', async () => {
-//     let vdcs = await getWards(URL, '5', '54', '5553');
-//     console.log(vdcs)
-//     expect(vdcs).toBeTruthy()
-// })
-
-// test('getElectionCentre', async () => {
-//     let vdcs = await getElectionCentres(URL, '5', '54', '5553', '8');
-//     console.log(vdcs)
-//     expect(vdcs).toBeTruthy()
-// })
-
-test('getVoterList', async () => {
-    let vdcs = await getVoterNames(URL, '5', '54', '5553', '8', '920');
+test('getVDC', async () => {
+    let vdcs = await getVDCs(URL, '5', '54');
     console.log(vdcs)
     expect(vdcs).toBeTruthy()
+})
+
+test('getWard', async () => {
+    let wards = await getWards(URL, '5', '54', '5553');
+    console.log(wards)
+    expect(wards).toBeTruthy()
+})
+
+test('getElectionCentre', async () => {
+    let centres = await getElectionCentres(URL, '5', '54', '5553', '8');
+    console.log(centres)
+    expect(centres).toBeTruthy()
+})
+
+test('getVoterList', async () => {
+    let voter_list = await getVoterNames(URL, '5', '54', '5553', '8', '920');
+    console.log(voter_list)
+    expect(voter_list).toBeTruthy()
 })
